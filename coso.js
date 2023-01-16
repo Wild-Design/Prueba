3f5879df92522e0d22d7bf86f8abb298b9dca7f4
GuidoGattini <gttnguido@gmail.com>
2023-01-16 18:12:17 -0300

    Merge renzo

--- coso.js
+++ coso.js
@@ -1,4 +1,4 @@
-//<<<<<<< HEAD
+
 const coso = ()=>{
     
 }
@@ -7,12 +7,12 @@ const Renzooo = ()=>{
     
 }
 
-//=======
+
 
 function Renzoo(){
     console.log ("pepe")
 }
-//>>>>>>> eea6c8c43591b5ff2b4336da54e26ca04ee24ac9
+
 
 const prueba = () =>{
     console.log("otra prueba mas")
